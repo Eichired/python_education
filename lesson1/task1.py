ccd3f8e8e1c493ef96b25589edbe175102e85a52
Eichi <alexandra.e.bogomolova@gmail.com>
2021-08-01 22:22:06 +0300

    Решена задача №2

---
+++ lesson1/task1.py
@@ -0,0 +1,13 @@
+a = 5
+b = 7
+c = "Одиннадцать"
+
+print(a)
+print(b)
+print(c, '\n')
+
+d = int(input("Введите цифру >>> "))
+e = float(input("Введите дробное число >>> "))
+f = input("Введите слово >>> ")
+
+print('', d, e, f, sep='\n')
