669bba04d3ac38a960e9dd067d4464a23127dc48
Eichired <alexandra.e.bogomolova@gmail.com>
2021-08-21 18:36:29 +0300

    Решена задача № 4

--- lesson5/task5_4.py
+++ lesson5/task5_4.py
@@ -15,7 +15,6 @@ def line_counter():
         x = []
         for el in range(len(data)):
             x.append(data[el].split())
-        print(x)
     with open('for_task4_output.txt', 'w') as out:
         for elem in range(len(x)):
             print(x[elem][0].replace(x[elem][0], d[elem+1]), x[elem][1], x[elem][2], file=out)
