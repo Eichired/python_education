61394598ad9de082b09e8abbeda954b1d2e0ba33
Eichi <alexandra.e.bogomolova@gmail.com>
2021-08-31 20:33:32 +0300

    Решена задача № 2

--- lesson7/task7_2.py
+++ lesson7/task7_2.py
@@ -10,37 +10,30 @@ from abc import ABC, abstractmethod
 
 
 class Clothes(ABC):
-
-    def __init__(self, param):
-        self.param = param
-
-    @property
-    def consumption(self):
-        return f'Количество затраченной ткани составляет: {self.param / 6.5 + 0.5 + 2 * self.param + 0.3 :.2f}'
-
     @abstractmethod
-    def abstract(self):
-        return 'Abstract 1'
-
-
-class Coat(Clothes):
-    def consumption(self):
-        return f'Для пошива пальто нужно: {self.param / 6.5 + 0.5 :.2f} метров ткани'
-
-    def abstract(self):
-        return 'Absract 2'
-
-
-class Costume(Clothes):
-    def consumption(self):
-        return f'Для пошива костюма нужно: {2 * self.param + 0.3 :.2f} метров ткани'
-
-    def abstract(self):
+    def expenses(self):
         pass
 
 
-coat = Coat(500)
-costume = Costume(500)
-print(coat.consumption())
-print(costume.consumption())
-print(coat.abstract())
+class Coat(Clothes):
+    def __init__(self, size=0):
+        self._size = size
+
+    @property
+    def expenses(self):
+        return round((self._size / 6.5) + 0.5, 2)
+
+
+class Costume(Clothes):
+    def __init__(self, height=0):
+        self._height = height
+
+    @property
+    def expenses(self):
+        return round(2 * self._height + 0.3, 2)
+
+quantity1 = Coat(100)
+quantity2 = Costume(100)
+print(f'Расходы на пальто: {quantity1.expenses}')
+print(f'Расходы на костюм: {quantity2.expenses}')
+print(f'Расходы на пальто и костюм: {quantity1.expenses + quantity2.expenses}')
